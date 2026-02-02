23a5b187341e9ea20ecab3d47d2dfda565e54953
max1m-ka <kolesnikov1us@icloud.com>
2026-02-02 20:27:09 +0300

    calculator.h

--- calculator/calculator.h
+++ calculator/calculator.h
@@ -1,1 +1,11 @@
-// В этом файле объявления функций.
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using Number = double;
+
+bool ReadNumber(Number& result);
+
+bool RunCalculatorCycle();
