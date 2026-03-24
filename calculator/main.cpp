76e47d43ad05d546f5ab022f9b4083cca90266d3
max1m-ka <kolesnikov1us@icloud.com>
2026-03-24 14:56:18 +0300

    Update main.cpp

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,6 +1,11 @@
-#include "calculator.h"
+#include "mainwindow.h"
 
-int main() {
-    RunCalculatorCycle();
-    return 0;
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
